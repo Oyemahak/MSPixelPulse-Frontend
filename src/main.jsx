518659605fecc3a4726cd3dde91643499f3d1f76
Mahak Patel <mahakpateluiux@gmail.com>
2026-08-09 19:46:47 -0400

    Load mobile blog refinements

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import "./styles/blog-mobile.css";
 import { AuthProvider } from "@/context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(

53d6582e2da90b69fa08088622b3bef8f4dcec60
Mahak Patel <mahakpateluiux@gmail.com>
2026-08-17 03:37:04 -0400

    fix: restore router and auth providers in frontend entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,27 @@
 // src/main.jsx
 
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
 import "./index.css";
+import "./styles/blog-mobile.css";
 
+import { AuthProvider } from "@/context/AuthContext.jsx";
 import { installApiReliability } from "./lib/apiReliability.js";
 
 installApiReliability();
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(
+  document.getElementById("root"),
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
